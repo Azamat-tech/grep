f1a36d55f00c9890a7bfeff47137c879cca91be8
Azamat Zarlykov <azarlykovcz@gmail.com>
2021-05-24 23:39:19 +0200

    started code was added

---
+++ grep.hs
@@ -0,0 +1,10 @@
+import System.Environment   
+import Data.List  
+
+main = do  
+    args <- getArgs                  -- IO [String]
+    progName <- getProgName          -- IO String
+    putStrLn "The arguments are:"  
+    mapM putStrLn args  
+    putStrLn "The program name is:"  
+    putStrLn progName
